3f0c1ba923d0a8829a4ed360ed4f7b1975561561
Pirmin Kalberer <pka@sourcepole.ch>
2014-04-04 13:56:33 +0200

    v0.8.7 Allow ERB in geodabase.yml

--- config/initializers/geodb.rb
+++ config/initializers/geodb.rb
@@ -1,7 +1,10 @@
 config_path = File.join(Rails.root,'config','geodatabase.yml')
 
-db_conf = File.exist?(config_path) ? YAML::load(File.open(config_path)) : {}
- 
+db_conf = {}
+if File.exist?(config_path)
+  db_conf = YAML.load(ERB.new(File.read(config_path)).result)
+end
+
 GEODB = db_conf[Rails.env]
 
 puts "Warning: No geodb configuration found in #{config_path}" if GEODB.nil?
